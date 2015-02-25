3ca5f65192a75326d284a5329bf17cf2eec9d157
Marcel Martin <marcel.martin@scilifelab.se>
2015-02-25 16:53:00 +0100

    Function for converting nt to aa

--- blastiger.py
+++ blastiger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-IgBLAST wrapper. Heavily inspired by igblastwrapper.
+An IgBLAST wrapper. Heavily inspired by igblastwrapper.
 
 Desired output is a table with the following columns:
 * read name
@@ -9,11 +9,13 @@ Desired output is a table with the following columns:
 * J gene
 * CDR3 sequence
 """
-from sqt import HelpfulArgumentParser
 import subprocess
 import sys
 import os
 from collections import namedtuple
+from sqt import HelpfulArgumentParser
+from sqt import SequenceReader
+from sqt.dna import GENETIC_CODE
 
 __author__ = "Marcel Martin"
 
@@ -21,6 +23,11 @@ __author__ = "Marcel Martin"
 IgblastRecord = namedtuple('IgblastRecord',
 	'query_name cdr3_start hits v_gene d_gene j_gene chain has_stop in_frame is_productive strand')
 Hit = namedtuple('Hit', 'query_id query_start query_sequence subject_start subject_sequence')
+
+
+def nt_to_aa(s):
+	"""Translate nucleotide sequence to amino acid sequence"""
+	return ''.join(GENETIC_CODE[s[i:i+3]] for i in range(0, len(s), 3))
 
 
 def run_igblast():
@@ -133,37 +140,9 @@ def parse_igblast_record(record_lines):
 
 def parse_igblast(path):
 	"""
-	This is how an IgBLAST "record" looks like (using -outfmt '7 qseqid qstart qseq sstart sseq'):
-	# IGBLASTN 2.2.29+
-# Query: M00559:99:000000000-ACGRF:1:1101:5380:6946;size=515;
-# Database: /proj/b2013006/sw/apps/igblastwrp-0.6/data/database/rhesus_monkey_IG_H_V /proj/b2013006/sw/apps/igblastwrp-0.6/data/database/rhesus_monkey_IG_H_D /proj/b2013006/sw/apps/igblastwrp
--0.6/data/database/rhesus_monkey_IG_H_J
-# Domain classification requested: imgt
+	Parse IgBLAST output, created with option -outfmt "7 qseqid qstart qseq sstart sseq"
 
-# V-(D)-J rearrangement summary for query sequence (Top V gene match, Top D gene match, Top J gene match, Chain type, stop codon, V-J frame, Productive, Strand).  Multiple equivalent top matches having the same score and percent identity, if present, are separated by a comma.
-IGHV4-2*01  IGHD4-1*01  IGHJ4*01    VH  No  In-frame    Yes +
-
-# V-(D)-J junction details based on top germline gene matches (V end, V-D junction, D region, D-J junction, J start).  Note that possible overlapping nucleotides at VDJ junction (i.e, nucleotides that could be assigned to either rearranging gene) are indicated in parentheses (i.e., (TACT)) but are not included under the V, D, or J gene itself
-TGCGA   AATA    TGACTACGGT  CCAAT   CTTTG
-
-# Alignment summary between query and top germline V gene hit (from, to, length, matches, mismatches, gaps, percent identity)
-FR1-IMGT    101 175 75  72  3   0   96
-CDR1-IMGT   176 199 24  18  6   0   75
-FR2-IMGT    200 250 51  45  6   0   88.2
-CDR2-IMGT   251 274 24  17  7   0   70.8
-FR3-IMGT    275 388 114 106 8   0   93
-CDR3-IMGT (germline)    389 392 4   4   0   0   100
-Total   N/A N/A 292 262 30  0   89.7
-
-# Hit table (the first field indicates the chain type of the hit)
-# Fields: query id, q. start, query seq, s. start, subject seq
-# 3 hits found
-V   M00559:99:000000000-ACGRF:1:1101:5380:6946;size=515 101 CAGGTCCAGCTGCAGGAGTCGGGCCCAGGACTGCTGAAGCCTTCGGAGACCCTGTCCCTCACCTGCGCTGTCTCTAGTGGCTCTTTCAGCAGTTACTGGTGGAACTGGATCCGCCAGTCCCCAGGGAAGGGACTGGAGTGGATTGGGGAAATCCATGGTAATAATGAGATCACCAACTATAACCCCTCCCTCAAGAGTCGAGTCACCATTTCAAAAGACGCGTCCAAGAAGCAGGTCTCCCTGAAGCTGAGTTCTGTGACCGCCGCGGACACGGCCGTGTATTTCTGTGCGA    1   CAGCTGCAGCTGCAGGAGTCGGGCCCAGGACTGGTGAAGCCTTCGGAGACCCTGTCCCTCACCTGCGCTGTCTCTGGTGGCTCCATCAGCAGTAACTACTGGAGCTGGATCCGCCAGCCCCCAGGGAAGGGACTGGAGTGGATTGGACGTATCTCTGGTAGTGGTGGGAGCACCGACTACAACCCCTCCCTCAAGAGTCGAGTCACCATTTCAACAGACACGTCCAAGAACCAGTTCTCCCTGAAGCTGAGCTCTGTGACCGCCGCGGACACGGCCGTGTATTACTGTGCGA
-D   M00559:99:000000000-ACGRF:1:1101:5380:6946;size=515 397 TGACTACGGT  1   TGACTACGGT
-J   M00559:99:000000000-ACGRF:1:1101:5380:6946;size=515 412 CTTTGACTTGTGGGGCCAGGGAGTCCTGGTCACCGTCTCC    5   CTTTGACTACTGGGGCCAGGGAGTCCTGGTCACCGTCTCC
-
-	last line in file:
-	# BLAST processed 14 queries
+	TODO add pident column
 	"""
 	with open(path) as f:
 		for record_header, record_lines in split_by_section(f, ['# IGBLASTN']):
@@ -174,6 +153,7 @@ J   M00559:99:000000000-ACGRF:1:1101:5380:6946;size=515 412 CTTTGACTTGTGGGGCCAGG
 def get_argument_parser():
 	parser = HelpfulArgumentParser(description=__doc__)
 	parser.add_argument('igblastout', help='output file of igblastn')
+	parser.add_argument('fasta', help='File with original reads')
 	return parser
 
 
