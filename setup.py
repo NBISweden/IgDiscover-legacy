8e238c6b82dbeb9b12e1f6cfc3595f36b23a4d01
Marcel Martin <marcel.martin@scilifelab.se>
2015-07-06 15:19:22 +0200

    Make databases dir a subdir of igypipe/ and add files to distribution

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
 	#license = 'MIT',
 	packages = ['igypipe'],
 	scripts = ['bin/igypipe'],
-	package_data = {'igypipe': ['pipeline.conf', 'Snakefile']},
+	package_data = {'igypipe': ['pipeline.conf', 'Snakefile', 'databases/*/*.fasta']},
 	install_requires = [
 		'sqt>=0.4.5',
 		'pandas>=0.16.2',
