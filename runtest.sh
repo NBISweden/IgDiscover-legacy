4641ed48d1118164f09c963d93a0f43cd5db31d4
Marcel Martin <marcel.martin@scilifelab.se>
2015-12-09 16:26:11 +0100

    Add an automated test

---
+++ runtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+unset DISPLAY
+
+export IGDATA=$HOME/scm/antibodies/igblast
+source venv/bin/activate
+rm -rf tmp/testrun
+igypipe init --library-name testing --db testdata/db --reads testdata/test.1.fastq.gz tmp/testrun
+cp -p testdata/pipeline.conf tmp/testrun/
+cd tmp/testrun
+snakemake -pj
