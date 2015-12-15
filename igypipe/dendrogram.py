75707bce7ddfdd576017686a97048d82ab03f586
Marcel Martin <marcel.martin@scilifelab.se>
2015-12-15 15:25:03 +0100

    In the dendrogram, mark new (discovered) sequences

--- igypipe/dendrogram.py
+++ igypipe/dendrogram.py
@@ -16,13 +16,27 @@ from igypipe.utils import distances
 logger = logging.getLogger(__name__)
 
 def add_arguments(parser):
+	parser.add_argument('--mark', '--db',
+		help='Path to a FASTA file with a set of "known" sequences. Sequences '
+		'in the main file that do *not* occur here will be marked with (new). '
+		'If not given, no sequences will be marked (use this to compare two '
+		'databases.')
 	parser.add_argument('fasta', help='Path to input FASTA file')
 	parser.add_argument('plot', help='Path to output PDF or PNG')
 
 
 def main(args):
-	sequences = list(FastaReader(args.fasta))
-	labels = [s.name for s in sequences]
+	with FastaReader(args.fasta) as fr:
+		sequences = list(fr)
+	if args.mark:
+		with FastaReader(args.mark) as fr:
+			mark = set(record.sequence for record in fr)
+		labels = [
+			record.name + (' (new)' if record.sequence not in mark else '')
+			for record in sequences
+		]
+	else:
+		labels = [s.name for s in sequences]
 	sns.set_style("white")
 	font_size = 297 / 25.4 * 72 / (len(labels) + 5)
 	font_size = min(16, max(6, font_size))
