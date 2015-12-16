13d1ab99602fc32aad2059cd4e3e68d1d2a460ec
Marcel Martin <marcel.martin@scilifelab.se>
2015-12-16 14:10:41 +0100

    Running "exec" makes it easier to cancel snakemake

--- runtest.sh
+++ runtest.sh
@@ -7,4 +7,4 @@ rm -rf testrun
 igypipe init --library-name testing --db testdata/db --reads testdata/reads.1.fastq.gz testrun
 cp -p testdata/igypipe.yaml testrun/
 cd testrun
-snakemake -pj
+exec snakemake -pj
