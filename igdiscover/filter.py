c5bbc00770f484bf84234e9d8ce9fce789be7536
Marcel Martin <marcel.martin@scilifelab.se>
2016-03-03 23:55:40 +0100

    Always log

--- igdiscover/filter.py
+++ igdiscover/filter.py
@@ -26,7 +26,6 @@ def filtered_table(table,
 		v_gene_coverage=90,  # at least
 		j_gene_coverage=60,  # at least
 		v_gene_evalue=1E-3,  # at most
-		log=False
 	):
 	"""
 	Discard the following rows in the table (read in by read_table):
@@ -40,30 +39,30 @@ def filtered_table(table,
 	"""
 	# Both V and J must be assigned
 	filtered = table.dropna(subset=('V_gene', 'J_gene'))[:]
-	if log: logger.info('%s rows have both V and J assignment', len(filtered))
+	logger.info('%s rows have both V and J assignment', len(filtered))
 	filtered['V_gene'] = pd.Categorical(filtered['V_gene'])
 
 	# Filter out sequences that have a stop codon
 	filtered = filtered[filtered.stop == 'no']
-	if log: logger.info('%s of those do not have a stop codon', len(filtered))
+	logger.info('%s of those do not have a stop codon', len(filtered))
 
 	# Filter out sequences with a too low V gene hit E-value
 	filtered = filtered[filtered.V_evalue <= v_gene_evalue]
-	if log: logger.info('%s of those have an E-value of at most %s', len(filtered), v_gene_evalue)
+	logger.info('%s of those have an E-value of at most %s', len(filtered), v_gene_evalue)
 
 	# Filter out sequences with too low V gene coverage
 	filtered = filtered[filtered.V_covered >= v_gene_coverage]
-	if log: logger.info('%s of those cover the V gene by at least %s%%', len(filtered), v_gene_coverage)
+	logger.info('%s of those cover the V gene by at least %s%%', len(filtered), v_gene_coverage)
 
 	# Filter out sequences with too low J gene coverage
 	filtered = filtered[filtered.J_covered >= j_gene_coverage]
-	if log: logger.info('%s of those cover the J gene by at least %s%%', len(filtered), j_gene_coverage)
+	logger.info('%s of those cover the J gene by at least %s%%', len(filtered), j_gene_coverage)
 
 	return filtered
 
 
 def main(args):
 	d = read_table(args.table, log=True)
-	d = filtered_table(d, log=True)
+	d = filtered_table(d)
 	print(d.to_csv(sep='\t', index=False), end='')
 	logger.info('%d rows written', len(d))
