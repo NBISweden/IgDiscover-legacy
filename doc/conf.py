555264d8ecdee250f254e3c245626c9174c856dc
Marcel Martin <marcel.martin@scilifelab.se>
2016-04-21 11:15:45 +0200

    disable fulltoc Sphinx extension (RTD has the same functionality)

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(os.pardir))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.fulltoc']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
