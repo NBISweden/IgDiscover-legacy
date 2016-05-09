d00492ad60a6f14e18418b856075a6237fdc3aea
Marcel Martin <marcel.martin@scilifelab.se>
2016-05-09 16:03:47 +0200

    IgDiscover is available under the MIT license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
 	author_email = 'marcel.martin@scilifelab.se',
 	url = '',
 	description = '',
-	#license = 'MIT',
+	license = 'MIT',
 	entry_points = {'console_scripts': ['igdiscover = igdiscover.__main__:main']},
 	packages = ['igdiscover'],
 	package_data = {'igdiscover': ['igdiscover.yaml', 'Snakefile', 'empty.aux']},
@@ -35,7 +35,7 @@ setup(
 		"Development Status :: 3 - Alpha",
 		"Environment :: Console",
 		"Intended Audience :: Science/Research",
-#		"License :: OSI Approved :: MIT License", (license undecided)
+		"License :: OSI Approved :: MIT License",
 		"Natural Language :: English",
 		"Programming Language :: Python :: 3",
 		"Topic :: Scientific/Engineering :: Bio-Informatics"
