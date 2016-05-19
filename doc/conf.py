5fbd559377a211454731459c22b5b8431856426f
Marcel Martin <marcel.martin@scilifelab.se>
2016-05-19 16:22:08 +0200

    Use local copy of fulltoc module in Sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -5,6 +5,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.abspath(os.pardir))
 
 # -- General configuration ------------------------------------------------
@@ -15,7 +16,7 @@ sys.path.insert(0, os.path.abspath(os.pardir))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.fulltoc']
+extensions = ['fulltoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
