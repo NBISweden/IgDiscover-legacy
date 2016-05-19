713afcd6379e7e5bfa33dc1d2f48867d597ade12
Marcel Martin <marcel.martin@scilifelab.se>
2016-05-19 16:22:56 +0200

    Use correct path names when testing with igdiscover-test-0.2 dataset

--- runtest.sh
+++ runtest.sh
@@ -4,7 +4,7 @@ unset DISPLAY
 export IGDATA=$PWD/igblast
 source venv/bin/activate
 rm -rf testrun
-igdiscover init --library-name testing --db testdata/db --reads testdata/reads.1.fastq.gz testrun
-cp -p testdata/igdiscover.yaml testrun/
+igdiscover init --library-name testing --db igdiscover-test-0.2/db --reads igdiscover-test-0.2/reads.1.fastq.gz testrun
+cp -p igdiscover-test-0.2/igdiscover.yaml testrun/
 cd testrun
 igdiscover run -p
