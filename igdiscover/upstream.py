11366150ceed5de5a9cd5b61de42a630fe3ae71b
Marcel Martin <marcel.martin@scilifelab.se>
2016-06-21 13:24:21 +0200

    Add some extra filtering options (such as --max-FR1-errors) to upstream subcommand

--- igdiscover/upstream.py
+++ igdiscover/upstream.py
@@ -1,7 +1,7 @@
 """
 Cluster upstream sequences (UTR and leader) for each gene
 
-For each gene, look at all the sequences assigned to it. Take the upstream
+For each gene, look at the sequences assigned to it. Take the upstream
 sequences and compute a consensus for them. Only those assigned sequences are
 taken that have a very low error rate for the V gene match.
 
@@ -9,7 +9,6 @@ Output a FASTA file that contains one consensus sequence for each gene.
 """
 import logging
 from collections import Counter
-from sqt.align import multialign, consensus
 
 from .table import read_table
 from .utils import iterative_consensus
@@ -20,48 +19,67 @@ logger = logging.getLogger(__name__)
 # this factor from the median length.
 UTR_MEDIAN_DEVIATION = 0.1
 
+
 def add_arguments(parser):
 	arg = parser.add_argument
-	arg('--max-error-percentage', '-e', metavar='PERCENT',
-		type=float, default=1,
-		help='Allow PERCENT errors in V gene match. Default: %(default)s%%.')
+	arg('--max-V-errors', '--max-error-percentage', '-e', dest='max_v_errors',
+	    metavar='PERCENT', type=float, default=1,
+		help='Allow PERCENT errors in V gene match. Default: %(default)s')
+	arg('--max-FR1-errors', dest='max_fr1_errors', metavar='PERCENT', type=float, default=None,
+	    help='Allow PERCENT errors in FR1 region.')
+	arg('--max-CDR1-errors', dest='max_cdr1_errors', metavar='PERCENT', type=float, default=None,
+	    help='Allow PERCENT errors in CDR1 region.')
+	arg('--min-consensus-size', type=int, default=1, metavar='N',
+	    help='Require at least N sequences for consensus. Default: %(default)s')
 	arg('--consensus-threshold', '-t', metavar='PERCENT',
 		type=float, default=75,
-		help='Threshold for consensus computation. Default: %(default)s%%.')
+		help='Threshold for consensus computation. Default: %(default)s%%')
+	arg('--no-ambiguous', '--no-N', default=False, action='store_true',
+	    help='Discard consensus sequences with ambiguous bases')
 	arg('--part', choices=['UTR', 'leader', 'UTR+leader'],
 		default='UTR+leader', help='Which part of the sequence before the V '
 		'gene match to analyze. Default: %(default)s')
 	arg('--debug', default=False, action='store_true',
 		help='Enable debugging output')
-	arg('table', help='Table with parsed IgBLAST results (usually filtered.tab.gz)')
+	arg('table', help='Table with parsed IgBLAST results (assigned.tab.gz or '
+		'filtered.tab.gz)')
 
 
 def main(args):
 	if args.debug:
 		logging.getLogger().setLevel(logging.DEBUG)
 	table = read_table(args.table)
-	logger.info('%s rows read', len(table))
-	table = table[table.V_SHM <= args.max_error_percentage]
-	logger.info('%s rows remain after discarding V%%SHM > %s%%', len(table), args.max_error_percentage)
+	n_genes = len(set(table['V_gene']))
+	logger.info('%s rows read with %d unique gene names', len(table), n_genes)
+	for name, column_name, arg in (
+			('V%SHM', 'V_SHM', args.max_v_errors),
+			('FR1%SHM', 'FR1_SHM', args.max_fr1_errors),
+			('CDR1%SHM', 'CDR1_SHM', args.max_cdr1_errors)):
+		if arg is not None:
+			table = table[getattr(table, column_name) <= arg]
+			logger.info('%s rows remain after discarding %s > %s%%',
+				len(table), name, arg)
 	table['UTR+leader'] = table['UTR'] + table['leader']
 	table = table[table[args.part] != '']
 	table['UTR_length'] = [ len(s) for s in table['UTR'] ]
 
-	n = 0
 	n_written = 0
 	n_consensus_with_n = 0
 	for name, group in table.groupby('V_gene'):
-		n += 1
 		assert len(group) != 0
+		if len(group) < args.min_consensus_size:
+			logger.info('Gene %s has too few assignments (%s), skipping.', name, len(group))
+			continue
+
 		counter = Counter(group['UTR_length'])
-		logger.debug('Sequence lengths (length: count): %s',  ', '.join('{}: {}'.format(l,c) for l, c in counter.most_common()))
+		logger.debug('Sequence lengths (length: count): %s', ', '.join('{}: {}'.format(l,c) for l, c in counter.most_common()))
 
 		if args.part == 'leader':
-			sequences = list(group[args.part])
+			sequences = list(group['leader'])
 		else:
-			# Take all sequences that are at least as long as the tenth longest
-			# sequence. This gives us at least ten usable sequences, enough for
-			# a consensus.
+			# Since UTR lengths vary a lot, take all sequences that are at
+			# least as long as the tenth longest sequence, and compute
+			# consensus only from those.
 			length_threshold = sorted(group['UTR_length'], reverse=True)[:10][-1]
 			sequences = list(group[group['UTR_length'] >= length_threshold][args.part])
 
@@ -81,7 +99,10 @@ def main(args):
 			len(set(sequences)), cons.count('N'))
 		if cons.count('N') > 0:
 			n_consensus_with_n += 1
+			if args.no_ambiguous:
+				continue
 		n_written += 1
 		print('>{} {}_consensus\n{}'.format(name, args.part, cons))
 
-	logger.info('Wrote a consensus for %s of %s genes (%s had N bases)', n_written, n, n_consensus_with_n)
+	in_or_ex = 'excluding' if args.no_ambiguous else 'including'
+	logger.info('Wrote a consensus for %s of %s genes (%s %s with ambiguous bases)', n_written, n_genes, in_or_ex, n_consensus_with_n)
