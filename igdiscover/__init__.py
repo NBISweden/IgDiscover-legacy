17ff2cac34f651a566dbbf61a89ffa6490dd432f
Marcel Martin <marcel.martin@scilifelab.se>
2016-06-30 19:10:58 +0200

    bump version

--- igdiscover/__init__.py
+++ igdiscover/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.9.dev0'  # setup.py relies on this being the first line in this file
+__version__ = '0.2.9.dev1'  # setup.py relies on this being the first line in this file
 
 from .table import read_table
