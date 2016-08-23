e84e5f65666adf2b925de75c667e5a96b2e317b3
Marcel Martin <marcel.martin@scilifelab.se>
2016-08-23 17:54:10 +0200

    Reduce default number of iterations to 3

--- igdiscover/utils.py
+++ igdiscover/utils.py
@@ -198,7 +198,7 @@ class Config:
 		self.minimum_merged_read_length = 300
 		self.mismatch_penalty = None
 		self.barcode_length = 0
-		self.iterations = 4
+		self.iterations = 3
 		self.ignore_j = False
 		self.subsample = 1000
 		self.stranded = False
