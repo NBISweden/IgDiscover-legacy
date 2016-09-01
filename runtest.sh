55ccc3a574ca1135dbb063498adf4480dd98e3c3
Marcel Martin <marcel.martin@scilifelab.se>
2016-09-01 21:56:24 +0200

    Test also with reads in single FASTA and FASTQ files

--- runtest.sh
+++ runtest.sh
@@ -3,8 +3,24 @@ set -eo pipefail
 unset DISPLAY
 export IGDATA=$PWD/igblast
 source venv/bin/activate
+
 rm -rf testrun
-igdiscover init --library-name=testing --db=testdata/db --reads testdata/reads.1.fastq.gz testrun
-cp -p testdata/igdiscover.yaml testrun/
-cd testrun
-igdiscover run -p
+mkdir testrun
+igdiscover init --library-name=testing --db=testdata/db --reads=testdata/reads.1.fastq.gz testrun/paired
+cp -p testdata/igdiscover.yaml testrun/paired/
+cd testrun/paired
+time igdiscover run -p
+cd ../..
+
+# Use the merged file from above as input again
+igdiscover init --library-name=testing --db=testdata/db --single-reads=testrun/paired/reads/2-merged.fastq.gz testrun/singlefastq
+cp -p testdata/igdiscover.yaml testrun/singlefastq/
+cd testrun/singlefastq
+time igdiscover run -p stats/reads.json
+cd ../..
+
+sqt fastxmod -w 0 --fasta testrun/paired/reads/2-merged.fastq.gz > testrun/reads.fasta
+igdiscover init --library-name=testing --db=testdata/db --single-reads=testrun/reads.fasta testrun/singlefasta
+cp -p testdata/igdiscover.yaml testrun/singlefasta/
+cd testrun/singlefasta
+time igdiscover run -p stats/reads.json
