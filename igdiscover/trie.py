9ae9c7fcda8188a8abc3984ebe0e6ea701d516cd
Marcel Martin <marcel.martin@scilifelab.se>
2017-05-12 17:52:30 +0200

    Implementation of a trie (keyword tree)
    It supports also querying for sequences that are similar to a given one.

---
+++ igdiscover/trie.py
@@ -0,0 +1,133 @@
+"""
+This trie can be used to store a set of strings and to retrieve sequences similar to a query
+sequence. 
+"""
+
+
+class Trie:
+	"""
+	A tree-like datastructure for storing strings. It supports queries similar to a set() of
+	strings. This particular implementation also supports searching for strings by Hamming distance.
+	"""
+	# Children
+	A = None
+	C = None
+	G = None
+	T = None
+	# The name attribute is set to the string that is spelled from the root of the tree to this
+	# node - but only for leaf nodes.
+	name = None
+
+	def add(self, s: str):
+		"""Add a string to this trie. If it already exists, the trie remains unchanged."""
+		self._insert(s, leaf_name=s)
+
+	def _insert(self, s: str, leaf_name: str):
+		"""Recursive insert, called by add()"""
+		if len(s) == 0:
+			# This needs to become a leaf node
+			assert self.name is None or self.name == leaf_name
+			self.name = leaf_name
+		else:
+			subtrie = getattr(self, s[0])
+			if subtrie is None:
+				subtrie = Trie()
+				setattr(self, s[0], subtrie)
+			subtrie._insert(s[1:], leaf_name)
+
+	def __repr__(self):
+		parts = []
+		for c in list('ACGT'):
+			if getattr(self, c) is not None:
+				if getattr(self, c) is None:
+					parts.append(c)
+				else:
+					parts.append(c + ':' + repr(getattr(self, c)))
+		if self.name is not None:
+			parts.append('Leaf({})'.format(self.name))
+		if parts:
+			return '{' + ', '.join(parts) + '}'
+		else:
+			return '*'
+
+	def find_node(self, s: str):
+		"""
+		Search for s. If s is a prefix of any string in the trie, then return the node
+		corresponding to that prefix. The node can be an internal or a leaf node.
+		A leaf node is a node in which name attribute is not None. If the returned node
+		is a leaf node, then s is in the trie. If it is an internal node (that is,
+		the name attribute is None), then there exists a string in the trie that has
+		s as a proper prefix.
+		
+		If no such string (with s as prefix) exists, then None is returned.
+		"""
+		if len(s) == 0:
+			return self
+		subtrie = getattr(self, s[0])
+		if subtrie is None:
+			return None
+		else:
+			return subtrie.find_node(s[1:])
+
+	def __contains__(self, s: str):
+		"""Return whether s is in the trie"""
+		node = self.find_node(s)
+		return node is not None and node.name == s
+
+	def count_nodes(self, internal=True):
+		"""Return number of nodes in this trie. internal nodes are counted if "internal" is True"""
+		n = 1 if internal or self.name is not None else 0
+		for c in list('ACGT'):
+			subtrie = getattr(self, c)
+			if subtrie is not None:
+				n += len(subtrie)
+		return n
+
+	def __len__(self):
+		"""Return the number of unique strings in this trie"""
+		return self.count_nodes(internal=False)
+
+	def has_similar(self, s, mismatches):
+		"""
+		Return whether a string exists in this trie that has a Hamming distance
+		of at most 'mismatches' to s.
+		"""
+		if len(s) == 0:
+			return self.name is not None
+		# As a runtime heuristic, descend into the the mismatches==0 subtrie first
+		# if possible.
+		subtrie = getattr(self, s[0])
+		if subtrie is not None and subtrie.has_similar(s[1:], mismatches):
+			return True
+		if mismatches == 0:
+			return False
+		# The above did not work - try all three possible substitutions
+		# and recursively check subtries
+		for c in list('ACGT'):
+			if c == s[0]:
+				continue
+			subtrie = getattr(self, c)
+			if subtrie is None:
+				continue
+			if subtrie.has_similar(s[1:], mismatches - 1):
+				return True
+		return False
+
+	def find_all_similar(self, s, mismatches):
+		"""
+		Yield all strings in this trie that have a Hamming distance of at most
+		'mismatches' to s.
+		"""
+		# This routine is similar to has_similar, but since we are interested
+		# in all similar strings, the optimization of going into the matching
+		# subtrie first does not help.
+		if len(s) == 0:
+			if self.name is not None:
+				yield self.name
+			return
+		for c in list('ACGT'):
+			if c != s[0] and mismatches == 0:
+				continue
+			subtrie = getattr(self, c)
+			if subtrie is not None:
+				yield from subtrie.find_all_similar(s[1:], mismatches - int(c != s[0]))
---
+++ tests/testtrie.py
@@ -0,0 +1,132 @@
+import random
+from igdiscover.trie import Trie
+from igdiscover.group import hamming_neighbors
+from sqt.align import hamming_distance
+
+
+def random_nt(length):
+	return ''.join(random.choice('ACGT') for _ in range(length))
+
+
+class TestTrie:
+	LENGTHS = (0, 1, 2, 3, 4, 5, 6, 10, 12, 15, 20)
+
+	def setup(self):
+		self.strings = set()
+		self.trie = Trie()
+		# Create a set and a Trie both containing the same strings
+		for _ in range(80):
+			for length in self.LENGTHS:
+				s = random_nt(length)
+				self.strings.add(s)
+				self.trie.add(s)
+
+	def test_empty_string(self):
+		trie = Trie()
+		trie.add('')
+		assert '' in trie
+		assert 'A' not in trie
+
+	def test_contains(self):
+		for s in self.strings:
+			assert s in self.trie
+
+		for length in self.LENGTHS:
+			for _ in range(min(100, 4**length)):
+				s = random_nt(length)
+				assert (s in self.strings) == (s in self.trie)
+
+	def test_len(self):
+		assert len(self.strings) == len(self.trie)
+
+	def naive_has_similar(self, s, distance):
+		for t in self.strings:
+			if len(t) != len(s):
+				continue
+			if hamming_distance(t, s) <= distance:
+				return True
+		return False
+
+	def naive_find_all_similar(self, s, distance):
+		for t in self.strings:
+			if len(t) != len(s):
+				continue
+			if hamming_distance(t, s) <= distance:
+				yield t
+
+	def test_has_similar(self):
+		for s in self.strings:
+			assert self.trie.has_similar(s, 0)
+			assert self.trie.has_similar(s, 1)
+			assert self.trie.has_similar(s, 2)
+
+		for base in self.strings:
+			for modified in hamming_neighbors(base):
+				assert self.trie.has_similar(modified, 1)
+
+		for errors in range(4):
+			for length in self.LENGTHS:
+				for _ in range(min(100, 4**length)):
+					s = random_nt(length)
+					assert self.naive_has_similar(s, errors) == self.trie.has_similar(s, errors)
+
+	def test_find_all_similar(self):
+		t = Trie()
+		t.add('ACGT')
+		result = list(t.find_all_similar('ACGG', 1))
+		assert set(result) == frozenset(('ACGT',))
+
+		for s in self.strings:
+			result = list(self.trie.find_all_similar(s, 0))
+			assert result == [s]
+
+			for errors in range(1, 4):
+				result = list(self.trie.find_all_similar(s, errors))
+				assert s in result
+
+		for base in self.strings:
+			for modified in hamming_neighbors(base):
+				assert base in self.trie.find_all_similar(modified, 1)
+
+		for errors in range(1, 3):
+			for length in self.LENGTHS:
+				for _ in range(min(100, 4**length)):
+					s = random_nt(length)
+					expected = set(self.naive_find_all_similar(s, errors))
+					assert expected == set(self.trie.find_all_similar(s, errors))
+
+
+def main():
+	import sys
+	n = int(sys.argv[1])
+	dist = int(sys.argv[2])
+
+	strings = [random_nt(13) for _ in range(n)]
+	print('created random sequences')
+
+	if sys.argv[3] == 'x':
+		def naive_has_similar(t):
+			for s in strings:
+				if hamming_distance(s, t) <= dist:
+					return True
+			return False
+
+		hs = 0
+		for s in strings:
+			hs += int(naive_has_similar(s[1:] + s[0]))
+		print('hs:', hs, 'out of', len(strings))
+
+	else:
+		trie = Trie()
+		for s in strings:
+			trie.add(s)
+		print('created trie')
+		hs = 0
+		for s in strings:
+			hs += int(trie.has_similar(s[1:] + s[0], dist))
+
+		print('hs:', hs, 'out of', len(strings))
+
+
+if __name__ == '__main__':
+	main()
