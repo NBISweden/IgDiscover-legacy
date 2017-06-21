3588628ae9c81b8585878e6961cfb5e1e47bf808
Marcel Martin <marcel.martin@scilifelab.se>
2017-06-21 14:43:10 +0200

    Require an activated conda env for runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
+# Run this within an activated igdiscover environment
+
 set -eo pipefail
 unset DISPLAY
-export IGDATA=$PWD/igblast
-source venv/bin/activate
 
 rm -rf testrun
 mkdir testrun
 igdiscover init --db=testdata/db --reads=testdata/reads.1.fastq.gz testrun/paired
 cp -p testdata/igdiscover.yaml testrun/paired/
-cd testrun/paired
-time igdiscover run -p
-cd ../..
+( cd testrun/paired && exec time -p igdiscover run -p )
 
 # Use the merged file from above as input again
 igdiscover init --db=testdata/db --single-reads=testrun/paired/reads/2-merged.fastq.gz testrun/singlefastq
 cp -p testdata/igdiscover.yaml testrun/singlefastq/
-cd testrun/singlefastq
-time igdiscover run -p stats/reads.json
-cd ../..
+( cd testrun/singlefastq && exec time -p igdiscover run -p stats/reads.json )
 
 sqt fastxmod -w 0 --fasta testrun/paired/reads/2-merged.fastq.gz > testrun/reads.fasta
 igdiscover init --db=testdata/db --single-reads=testrun/reads.fasta testrun/singlefasta
 cp -p testdata/igdiscover.yaml testrun/singlefasta/
-cd testrun/singlefasta
-time igdiscover run -p stats/reads.json
+( cd testrun/singlefasta && exec time -p igdiscover run -p stats/reads.json )
