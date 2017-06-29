0382c83bdc5f733023c5ee345d6d57e396d39e40
Marcel Martin <marcel.martin@scilifelab.se>
2017-06-29 10:43:04 +0200

    Use discovered J database in subsequent iterations
    Just like for V genes, the J genes discovered in iteration N are now used as
    input J database for iteration N+1.
    
    Closes #64

--- runtest.sh
+++ runtest.sh
@@ -7,15 +7,25 @@ unset DISPLAY
 rm -rf testrun
 mkdir testrun
 igdiscover init --db=testdata/db --reads=testdata/reads.1.fastq.gz testrun/paired
-cp -p testdata/igdiscover.yaml testrun/paired/
-( cd testrun/paired && exec time -p igdiscover run -p )
+
+cd testrun/paired
+igdiscover config --set iterations 1 \
+   --set race_g true \
+   --set stranded true \
+   --set barcode_length_5prime 12 \
+   --set reverse_primers '["GCAGGCCTTTTTGGCCNNNNNGGGGCATTCTCACAGGAGACGAGGGGGAAAAG"]' \
+   --set forward_primers '["CGTGAGCTGAGTACGACTCACTATAGCTTCAC"]' \
+   --set germline_filter.unique_js 1
+time -p igdiscover run -p
+cd ../..
 
 # Use the merged file from above as input again
 igdiscover init --db=testdata/db --single-reads=testrun/paired/reads/2-merged.fastq.gz testrun/singlefastq
-cp -p testdata/igdiscover.yaml testrun/singlefastq/
+cp -p testrun/paired/igdiscover.yaml testrun/singlefastq/
 ( cd testrun/singlefastq && exec time -p igdiscover run -p stats/reads.json )
 
+# Test FASTA input
 sqt fastxmod -w 0 --fasta testrun/paired/reads/2-merged.fastq.gz > testrun/reads.fasta
 igdiscover init --db=testdata/db --single-reads=testrun/reads.fasta testrun/singlefasta
-cp -p testdata/igdiscover.yaml testrun/singlefasta/
+cp -p testrun/paired/igdiscover.yaml testrun/singlefastq/
 ( cd testrun/singlefasta && exec time -p igdiscover run -p stats/reads.json )
