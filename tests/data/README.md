2e663fea5c1c4b6e2109fe8b462831bc55664fd3
Marcel Martin <marcel.martin@scilifelab.se>
2018-03-27 15:01:30 +0200

    Run clusterplot as part of tests
    An IgDiscover installation is currently failing because matplotlib 2.2 is
    installed, which we’re not compatible with (or possibly seaborn). This test
    will catch this problem in the future.

---
+++ tests/data/README.md
@@ -0,0 +1,8 @@
+# clusterplot test data
+
+The `clusterplot.tab.gz` file was extracted from the SRR5408020 dataset with this command:
+
+    (
+      zcat iteration-01/filtered.tab.gz | head -n 1
+      zcat iteration-01/filtered.tab.gz | awk -vFS='\t' '$19==0' | grep '\bIGHV1-18\*01\b' | head -n 10
+    ) | cut -f2,19,38 | gzip > clusterplot.tab
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -42,3 +42,8 @@ def test_group_by_pseudo_cdr3():
 def test_group_by_pseudo_cdr3_barcode_at_end():
 	args = ['group', '-b', '-4', '--pseudo-cdr3=1:3', datapath('ungrouped.fasta')]
 	run(args, resultpath('grouped2.fasta'))
+
+
+def test_clusterplot(tmpdir):
+	main(['clusterplot', '-m', '10', datapath('clusterplot.tab.gz'), str(tmpdir)])
+	assert tmpdir.join('IGHV1-1801.png').check()
