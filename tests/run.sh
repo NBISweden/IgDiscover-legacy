77a5e6e55a51f88e0a3808bb1f8d90e4b78727e8
Marcel Martin <marcel.martin@scilifelab.se>
2018-06-27 00:06:17 +0200

    Run tests on new v0.5 dataset

--- tests/run.sh
+++ tests/run.sh
@@ -7,30 +7,21 @@ pytest
 
 rm -rf testrun
 mkdir testrun
-[[ -L testdata ]] || ln -s igdiscover-testdata-* testdata
+[[ -L testdata ]] || ln -s igdiscover-testdata testdata
 
-igdiscover init --db=testdata/db --reads=testdata/reads.1.fastq.gz testrun/paired
+igdiscover init --db=testdata/database --reads=testdata/reads.1.fastq.gz testrun/paired
 
 cd testrun/paired
-igdiscover config --set iterations 2 \
-   --set race_g true \
-   --set stranded true \
-   --set barcode_length_5prime 12 \
-   --set cdr3_location '[-80, -60]' \
-   --set reverse_primers '["GCAGGCCTTTTTGGCCNNNNNGGGGCATTCTCACAGGAGACGAGGGGGAAAAG"]' \
-   --set forward_primers '["CGTGAGCTGAGTACGACTCACTATAGCTTCAC"]' \
-   --set germline_filter.unique_js 1 \
-   --set j_discovery.propagate false
 igdiscover run
 cd ../..
 
 # Use the merged file from above as input again
-igdiscover init --db=testdata/db --single-reads=testrun/paired/reads/2-merged.fastq.gz testrun/singlefastq
+igdiscover init --db=testdata/database --single-reads=testrun/paired/reads/2-merged.fastq.gz testrun/singlefastq
 cp -p testrun/paired/igdiscover.yaml testrun/singlefastq/
 ( cd testrun/singlefastq && igdiscover run stats/reads.json )
 
 # Test FASTA input
 sqt fastxmod -w 0 --fasta testrun/paired/reads/2-merged.fastq.gz > testrun/reads.fasta
-igdiscover init --db=testdata/db --single-reads=testrun/reads.fasta testrun/singlefasta
+igdiscover init --db=testdata/database --single-reads=testrun/reads.fasta testrun/singlefasta
 cp -p testrun/paired/igdiscover.yaml testrun/singlefastq/
 ( cd testrun/singlefasta && igdiscover run stats/reads.json )
