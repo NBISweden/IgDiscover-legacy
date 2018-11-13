81690e885b3f82bbfadb1fbe1c42fe13c1fb32d9
Marcel Martin <marcel.martin@scilifelab.se>
2018-11-13 11:25:14 +0100

    Remove unused code

--- igdiscover/dbdiff.py
+++ igdiscover/dbdiff.py
@@ -14,84 +14,8 @@ do_not_show_cpustats = 1
 
 def add_arguments(parser):
 	arg = parser.add_argument
-	# arg('--no-sort', dest='sort', action='store_false', default=True,
-	# 	help='Do not sort sequences by name')
-	# arg('--not-found', metavar='TEXT', default=' (not found)',
-	# 	help='Append this text to the record name when the sequence was not found '
-	# 	'in the template. Default: %(default)r')
-	# arg('--rename-from', metavar='TEMPLATE',
-	# 	help='FASTA template file with correctly named sequences. If a sequence in '
-	# 		'the target file is identical to one in the template, it is assigned the '
-	# 		'name of the sequence in the template.')
-	# arg('--order-by', metavar='TEMPLATE',
-	# 	help='FASTA template that contains genes in the desired order. '
-	# 		'If a name contains a "*" (asterisk), only the preceding part '
-	# 		'is used. Thus, VH4-4*01 and VH4-4*02 are equivalent.')
 	arg('a', help='FASTA file with expected sequences')
 	arg('b', help='FASTA file with actual sequences')
-
-
-class PrefixDict:
-	"""
-	A dict that maps strings to values, but where a prefix of a key is enough
-	to retrieve the value.
-	"""
-	def __init__(self, items):
-		self._items = []
-		for k, v in items:
-			self.add(k, v)
-
-	def add(self, k, v):
-		try:
-			self[k]
-			raise ValueError('Key {!r} already exists'.format(k))
-		except KeyError:
-			self._items.append((k, v))
-
-	def __getitem__(self, key):
-		found = None
-		for seq, value in self._items:
-			if seq.startswith(key) or key.startswith(seq):
-				if found is not None:
-					# TODO don't use keyerror here
-					raise KeyError('Key {!r} is ambiguous'.format(key))
-				found = value
-		if found is None:
-			raise KeyError(key)
-		return found
-
-	def get(self, key, default=None):
-		try:
-			v = self[key]
-		except KeyError:
-			return default
-		else:
-			return v
-
-	def __len__(self):
-		return len(self._items)
-
-
-class GeneMissing(Exception):
-	pass
-
-
-def gene_name(record):
-	record_name, _, record_comment = record.name.partition(' ')
-	return record_name.partition('*')[0]
-
-
-def sorted_by_gene(records, gene_order):
-	d = {name: i for i, name in enumerate(gene_order)}
-
-	def keyfunc(record):
-		gene = gene_name(record)
-		try:
-			return d[gene]
-		except KeyError:
-			raise GeneMissing(gene)
-
-	return sorted(records, key=keyfunc)
 
 
 def check_duplicate_names(records):
