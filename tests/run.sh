8c8074e16935c0117d3b80af8f5d1167194d2991
Marcel Martin <marcel.martin@scilifelab.se>
2018-11-26 13:14:14 +0100

    Test whether specifying primer sequences leads to a SyntaxError
    Snakemake version 5.0 and newer complain about the trim_forward_primers
    rule with this error message:
    
    SyntaxError:
    Not all output, log and benchmark files of rule trim_forward_primers contain the same
    wildcards. This is crucial though, in order to avoid that two or more jobs write to the
    same file.
    
    The error is only triggered if forward_primers are defined in the
    configuration.
    
    See issue #93

--- tests/run.sh
+++ tests/run.sh
@@ -10,8 +10,18 @@ rm -rf testrun
 mkdir testrun
 [[ -L testdata ]] || ln -s igdiscover-testdata testdata
 
-igdiscover init --db=testdata/database --reads=testdata/reads.1.fastq.gz testrun/paired
 
+# Test whether specifying primer sequences leads to a SyntaxError
+igdiscover init --db=testdata/database --reads=testdata/reads.1.fastq.gz testrun/primers
+pushd testrun/primers
+igdiscover config \
+	--set forward_primers "['CGTGA']" \
+	--set reverse_primers "['TTCAC']"
+igdiscover run -n stats/reads.json
+popd
+
+
+igdiscover init --db=testdata/database --reads=testdata/reads.1.fastq.gz testrun/paired
 pushd testrun/paired
 igdiscover config --set barcode_length_3prime 21
 
