a13110dc27bc8dec06944897791058a5811d4435
Marcel Martin <marcel.martin@scilifelab.se>
2019-04-09 16:37:32 +0200

    Test the "igblast" subcommand
    Add a tiny test file with VDJ sequences and expected results.
    
    Also needed to add a .gitattributes to nsure CR-LF line endings are preserved
    for the .tab file.

--- tests/data/README.md
+++ tests/data/README.md
@@ -6,3 +6,10 @@ The `clusterplot.tab.gz` file was extracted from the SRR5408020 dataset with thi
       zcat iteration-01/filtered.tab.gz | head -n 1
       zcat iteration-01/filtered.tab.gz | awk -vFS='\t' '$19==0' | grep '\bIGHV1-18\*01\b' | head -n 10
     ) | cut -f2,19,38 | gzip > clusterplot.tab
+
+# IgBLAST parsing
+
+igblast.fasta is a test file for IgBLAST parsing with some corner cases (V incomplete at 5'
+end, no D assigned etc.). The database for this is in database/.
+
+
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,19 +1,19 @@
 import os
 import sys
-from tempfile import TemporaryDirectory
 import pytest
 
 from igdiscover.__main__ import main
 from .utils import datapath, resultpath, files_equal
 
 
-def run(args, expected):
-    """
-    Run IgDiscover, redirecting stdout to a temporary file.
-    Then compare the output with the contents of an expected file.
-    """
-    with TemporaryDirectory() as td:
-        outpath = os.path.join(td, 'output')
+@pytest.fixture
+def run(tmpdir):
+    def _run(args, expected):
+        """
+        Run IgDiscover, redirecting stdout to a temporary file.
+        Then compare the output with the contents of an expected file.
+        """
+        outpath = str(tmpdir.join('output'))
         print('Running:', ' '.join(args))
         with open(outpath, 'w') as f:
             old_stdout = sys.stdout
@@ -22,6 +22,8 @@ def run(args, expected):
             sys.stdout = old_stdout
         assert files_equal(expected, outpath)
 
+    return _run
+
 
 def test_main():
     with pytest.raises(SystemExit) as exc:
@@ -29,17 +31,17 @@ def test_main():
     assert exc.value.code == 0
 
 
-def test_group_by_barcode_only():
+def test_group_by_barcode_only(run):
     args = ['group', '-b', '4', datapath('ungrouped.fasta')]
     run(args,  resultpath('grouped-by-barcode-only.fasta'))
 
 
-def test_group_by_pseudo_cdr3():
+def test_group_by_pseudo_cdr3(run):
     args = ['group', '-b', '4', '--pseudo-cdr3=-5:-2', '--trim-g', datapath('ungrouped.fasta')]
     run(args,  resultpath('grouped.fasta'))
 
 
-def test_group_by_pseudo_cdr3_barcode_at_end():
+def test_group_by_pseudo_cdr3_barcode_at_end(run):
     args = ['group', '-b', '-4', '--pseudo-cdr3=1:3', datapath('ungrouped.fasta')]
     run(args, resultpath('grouped2.fasta'))
 
@@ -47,3 +49,8 @@ def test_group_by_pseudo_cdr3_barcode_at_end():
 def test_clusterplot(tmpdir):
     main(['clusterplot', '-m', '10', datapath('clusterplot.tab.gz'), str(tmpdir)])
     assert tmpdir.join('IGHV1-1801.png').check()
+
+
+def test_igblast(run):
+    args = ['igblast', datapath('database/'), datapath('igblast.fasta')]
+    run(args, resultpath('assigned.tab'))
