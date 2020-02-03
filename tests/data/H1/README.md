ca8d706142879eb567fbdcd45203eb1c351c03b1
Marcel Martin <marcel.martin@scilifelab.se>
2020-02-03 17:49:20 +0100

    Delete unused directory

--- tests/data/H1/README.md
+++
@@ -1,4 +0,0 @@
-These are partial results from ERR1760498.
-
-V.fasta -- V gene sequences that actually appear in the `candidates.tab` (not
-the full V starting database).
--- tests/data/H1/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-igdiscover germlinefilter --whitelist=V.fasta --max-differences=0 --unique-CDR3=5 --cluster-size=100 --unique-J=3 --cross-mapping-ratio=0.02 --allele-ratio=0.1 candidates.tab.gz > new_V_germline.tab
-diff -U 0 <(cut -f1 expected.tab) <(cut -f1 new_V_germline.tab) | grep '^[+-]' | sed 1,2d
